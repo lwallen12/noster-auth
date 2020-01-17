595d6ecd7ac82a3d9a38286a74444c6ddb065880
will allen <william.allen1296@gmail.com>
2020-01-17 14:36:49 -0600

    Now basic auth and authgaurd seem to be working just fine

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { InsideComponent } from './inside/inside.component';
+import { AuthGuard } from './auth/AuthGuard';
 
 const routes: Routes = [
   {path: '', redirectTo: '/auth', pathMatch: 'full'},
   {path: 'auth', component: AuthComponent},
-  {path: 'inside', component: InsideComponent}
+  {path: 'inside', component: InsideComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
