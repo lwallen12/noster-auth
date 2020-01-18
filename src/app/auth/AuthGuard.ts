410260d9f26d14cd2861a9ae2250a379c96c96c1
will allen <william.allen1296@gmail.com>
2020-01-18 11:18:05 -0600

    just re commiting at new start point

--- src/app/auth/AuthGuard.ts
+++ src/app/auth/AuthGuard.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../auth.service';
 
+import * as jwt_decode from "jwt-decode";
+
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
     constructor(
@@ -13,7 +15,7 @@ export class AuthGuard implements CanActivate {
         const currentUser = this.authenticationService.currentUserValue;
 
 
-        if (currentUser)  {
+        if (currentUser && this.getExpirations())  {
             // authorised so return true
             return true;
         }
@@ -23,4 +25,27 @@ export class AuthGuard implements CanActivate {
         this.router.navigate(['/auth']);
         return false;
     }
+
+    getExpirations() {
+        let now = Date.now();
+        let decoded = this.decodeThis(this.authenticationService.currentUserValue);
+    
+        let expiresIn = decoded.exp * 1000;
+    
+        console.log(Date.now());
+        console.log(decoded.exp * 1000);
+    
+        if (expiresIn > now) {
+            return true;
+        } else {
+            return false;
+        }
+
+      }
+
+
+    decodeThis(token) {
+        let decoded = jwt_decode(token);
+        return decoded;
+      }
 }
